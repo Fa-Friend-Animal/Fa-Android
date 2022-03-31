958ed0829df3e8ab633980dd49932375bd4c7e7d
ckrudals <2004ckck@naver.com>
2022-03-31 23:10:03 +0900

    ⚰️ :: delete Navigation class description : Duplicate features

--- app/src/main/java/com/example/fa_android/util/Navigation.kt
+++ app/src/main/java/com/example/fa_android/util/Navigation.kt
@@ -5,6 +5,7 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.fa_android.feature.login.screen.Login
 import com.example.fa_android.feature.main.Main
 //import com.example.fa_android.feature.login.screen.Login
 
@@ -17,7 +18,7 @@ fun Navigation() {
     val navController = rememberNavController()
     NavHost(navController = navController, startDestination = Screen.LoginScreen.route) {
         composable(route = Screen.LoginScreen.route) {
-//            Login(navController = navController)
+            Login(navController = navController)
 //            Main(navController = navController)
         }
     }
