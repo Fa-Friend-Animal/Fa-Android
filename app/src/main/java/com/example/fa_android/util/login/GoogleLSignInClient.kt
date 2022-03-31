4fe5ad75eb95e25c232968cee8f502f52441e424
ckrudals <2004ckck@naver.com>
2022-03-31 22:57:01 +0900

    ⚡️ :: update login logic description : GoogleSignIn logic

---
+++ app/src/main/java/com/example/fa_android/util/login/GoogleLSignInClient.kt
@@ -0,0 +1,16 @@
+package com.example.fa_android.util.login
+
+import android.content.Context
+import com.example.fa_android.R
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+
+fun getGoogleSignInClient(context: Context): GoogleSignInClient {
+    val signInOptions = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+        .requestIdToken(context.getString(R.string.web_client_id))
+        .requestEmail()
+        .build()
+
+    return GoogleSignIn.getClient(context, signInOptions)
+}
