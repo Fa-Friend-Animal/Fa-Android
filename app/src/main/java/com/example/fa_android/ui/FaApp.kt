28e386fad26ee40859cffb607c398b1c4767e5d7
ckrudals <2004ckck@naver.com>
2022-03-31 23:46:05 +0900

    ⚡️ :: delete different branch code

--- app/src/main/java/com/example/fa_android/ui/FaApp.kt
+++ app/src/main/java/com/example/fa_android/ui/FaApp.kt
@@ -3,14 +3,8 @@ package com.example.fa_android.ui
 import android.app.Application
 import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.runtime.Composable
-import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import androidx.navigation.navArgument
-import com.example.fa_android.di.loginModule
-import com.example.fa_android.feature.login.screen.Login
-import com.example.fa_android.feature.main.Main
 import com.example.fa_android.util.Screen
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
@@ -46,9 +40,8 @@ fun FaApp() {
 //            darkIcons = true
 //        )
 //    }
-    NavHost(navController = navController, startDestination = Screen.LoginScreen.route) {
-//        composable(Screen.LoginScreen.route) { Login(navController = navController) }
-//        composable(route = Screen.MainScreen.route,) { Main(navController = navController) }
+    NavHost(navController = navController, startDestination = "") {
+
 
     }
 }
@@ -61,7 +54,6 @@ class FaApplication : Application() {
         startKoin {
             androidLogger()
             androidContext(this@FaApplication)
-//            modules(loginModule)
         }
 
     }
